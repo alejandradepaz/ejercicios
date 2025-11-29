015acb587c140a58b4d56d14340f3cba6344e85d
Aleja <depazruiz.alejandra@gmail.com>
2025-11-29 17:15:23 -0500

    se agrega boton2

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ led_boton = Pin(5, Pin.OUT)
 # --- Botón con pull-up interno ---
 boton = Pin(4, Pin.IN, Pin.PULL_UP)
 
+boton2 = Pin(18, Pin.IN, Pin.PULL_UP)
+prev_boton2 = 1  
+
 # --- HILO PARA EL PARPADEO AUTOMÁTICO ---
 def parpadeo():
     while True:
@@ -27,10 +30,16 @@ _thread.start_new_thread(parpadeo, ())
 # --- BUCLE PRINCIPAL (BOTÓN) ---
 while True:
     if boton.value() == 1:
-        print("Boton encendido")# Botón presionado (va a GND)
+        #print("Boton encendido")# Botón presionado (va a GND)
         led_boton.value(1)       # Enciende LED
     else:
         #print("Boton apagado")
         led_boton.value(0)       # Apaga LED
 
     time.sleep(0.01)
+    
+    valor_b2 = boton2.value()
+    
+    if prev_boton2 == 1 and valor_b2 == 0:
+        print("Botón 2 PRESIONADO")  
+    prev_boton2 = valor_b2
